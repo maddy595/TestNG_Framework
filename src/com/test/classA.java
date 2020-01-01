b329cf2026fff04f394a98f73f3a0dcccbaa421e
maddy595 <madhav.gaikwad@BDC7-L-72000NF.dir.svc.accenture.com>
2020-01-01 23:23:12 +0530

    changed testng xml file

--- src/com/test/classA.java
+++ src/com/test/classA.java
@@ -1,8 +1,24 @@
 package com.test;
 
+import org.testng.annotations.Test;
+
 public class classA {
 	
 	
+	@Test
+	public void samplemethod1() {
+		System.out.println("Hello1");
+	}
+	
+	@Test
+	public void samplemethod2() {
+		System.out.println("Hello2");
+	}
+	
+	@Test
+	public void samplemethod3() {
+		System.out.println("Hello3");
+	}
 
 
 }
